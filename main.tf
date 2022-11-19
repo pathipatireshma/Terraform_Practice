08d36b2c519649c348bcf669f3181b45a1b06275
Reshma <pathipati.reshma@gmail.com>
2022-11-19 21:34:54 +0530

    commit

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ resource "aws_vpc" "my_test_vpc" {
     }
 }
 resource "aws_subnet" "subnet_a" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     cidr_block = "10.0.0.0/26"
     tags = {
       "Name" = "subnet_a"
     }
 }
 resource "aws_subnet" "subnet_b" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     cidr_block = "10.0.0.64/26"
     tags = {
       "Name" = "subnet_b"
@@ -24,7 +24,7 @@ resource "aws_subnet" "subnet_b" {
 }
 
 resource "aws_subnet" "subnet_c" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     cidr_block = "10.0.0.128/26"
     tags = {
       "Name" = "subnet_c"
@@ -32,7 +32,7 @@ resource "aws_subnet" "subnet_c" {
 }
 
 resource "aws_subnet" "subnet_d" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     cidr_block = "10.0.0.192/26"
     tags = {
         "Name" = "subnet_d"
@@ -40,13 +40,13 @@ resource "aws_subnet" "subnet_d" {
 }
 
 resource "aws_internet_gateway" "my_Igway" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     tags = {
         Name = "my_Igway"
     }
 }
 resource "aws_route_table" "my_test_route" {
-    vpc_id = aws_vpc.my_test_vpc.id
+    vpc_id = var.vpc
     route = []
     tags = {
         Name = "my_test_route"
@@ -65,8 +65,7 @@ resource "aws_route_table_association" "b" {
 }
 
 output "vpc_id" {
-    value = aws_vpc.my_test_vpc.id
-
+    value =  var.vpc
 }
 output "aws_internet_gateway" {
     value = aws_internet_gateway.my_Igway.id
---
+++ vars.tf
@@ -0,0 +1,4 @@
+variable "vpc" {
+    vpc_id = aws_vpc.my_test_vpc.id
+  
+}
