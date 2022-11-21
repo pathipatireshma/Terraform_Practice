6d76d9b6f7cd6db1b823fa8bfed43524e135d1eb
Reshma <pathipati.reshma@gmail.com>
2022-11-21 16:39:31 +0530

    commit

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "reshma-cloud-practice"
-        key = "tfstatefiles/"
+        key = "tfstatefiles/terraform.tfstate"
         region = "us-east-1"
 
       
