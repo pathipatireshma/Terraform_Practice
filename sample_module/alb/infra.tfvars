c631226b70aef5f533bfd4a51cfcb88c19312418
Reshma <pathipati.reshma@gmail.com>
2022-11-24 14:26:02 +0530

    commit

--- sample_module/alb/infra.tfvars
+++ sample_module/alb/infra.tfvars
@@ -1,5 +1,6 @@
 my_vpc = "vpc-0b962b8dfc23fff96"
-subnet_id = ["subnet-02470c5e30cf1098d","subnet-07173b6494aacb1cb","subnet-07ec79d828981a1f8","subnet-04edea347b0fe46cf"]
+subnet_id = ["subnet-02470c5e30cf1098d"]
+# ,"subnet-07173b6494aacb1cb","subnet-07ec79d828981a1f8","subnet-04edea347b0fe46cf"]
 port = 80
 protocol = "HTTP"
 target = "instance"
--- sample_module/alb/main.tf
+++ sample_module/alb/main.tf
@@ -2,12 +2,12 @@ module "test_alb" {
     # source = "sample_module/aws_lb/my_test_lb"
     source = "../"
     # name = "app-lb"
-    # load_balancer_type = "application"
+   
     my_vpc = var.my_vpc
-    # subnet_id =   var.subnet_id
-    count = length(var.subnet_id)
-    subnets = var.subnet_id[count.index]
-    # enable_cross_zone_load_balancing = false
+    subnet_id =   var.subnet_id
+    # count = length(var.subnet_id)
+    # subnets = var.subnet_id[count.index]
+   
     port = var.port
     protocol = var.protocol
     target = var.target
